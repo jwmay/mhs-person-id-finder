110aea972634a21c5882e117f3472032bcde3843
Joseph May <jwmay87@gmail.com>
2015-12-28 21:14:32 -0800

    use last name and dob to verify user

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,21 +1,23 @@
 (function() {
     var app = angular.module('personIdFinder', ['firebase']);
 
-    app.controller('LookupController', function($timeout) {
+    app.controller('LookupController', function($timeout, $filter) {
         var _this = this;
         
         // Usage instructions to display to user
         var instructions = 'Enter your last name, student number, and date of birth above to find your username';
         
-        // Flag to control display of the spinning wheel
+        // Flag to control display of the spinning wheel while searching and loading user data
         this.searching = false;
         
         // Object to store the user info, a flag to display the user info, and a message before
         // the first search is initiated
         this.user = {
             firstName: '',
+            lastName: '',
             studentNumber: '',
             personId: '',
+            birthdate: '',
             found: false,
             message: instructions
         };
@@ -47,7 +49,25 @@
                     // Display resuts if found in the Firebase database, otherwise, display a message
                     if (data !== null && data !== undefined) {
                         for (var key in data) {
-                            _updateUser(data[key].personId);
+                            // Extract data
+                            var firstName = data[key].firstName;
+                            var lastName = data[key].lastName;
+                            var personId = data[key].personId;
+                            var dob = $filter('mhsDateFilter')(data[key].birthdate);
+                            
+                            // Verifty that the last name matches
+                            if ($filter('lowercase')(_this.user.lastName) === $filter('lowercase')(lastName)) {
+                                // Verify that the birthdate matches
+                                if (_this.user.birthdate === dob) {
+                                    _updateUser(firstName, personId);
+                                }
+                                else {
+                                    _this.user.message = 'Date of birth does not match record';
+                                }
+                            }
+                            else {
+                                _this.user.message = 'Last name does not match record';
+                            }
                         }
                     }
                     else {
@@ -64,14 +84,17 @@
         // Clear the input fields and the data stored in the user object
         this.reset = function() {
             _this.user.firstName = '';
+            _this.user.lastName = '';
             _this.user.studentNumber = '';
             _this.user.personId = '';
+            _this.user.birthdate = '';
             _this.user.found = false;
             _this.user.message = instructions;
         };
         
         // Function to update the person ID once found in the Firebase database
-        var _updateUser = function(personId) {
+        var _updateUser = function(firstName, personId) {
+            _this.user.firstName = firstName;
             _this.user.personId = personId;
             _this.user.found = true;
         };
@@ -92,6 +115,20 @@
             // Convert all characters to lowercase
             var lowercaseName = $filter('lowercase')(noSpecialChars);
             return lowercaseName;
+        };
+    });
+    
+    // Filter the birthdate string from MM/DD/YYYY to MMDDYY
+    app.filter('mhsDateFilter', function() {
+        return function(date) {
+            // Remove the slashes
+            var noSlashes = date.split('/');
+
+            // Create 2-digit year            
+            noSlashes[2] = noSlashes[2].slice(2);
+            
+            // Return the 6-digit date as a string
+            return noSlashes.join('');
         };
     });
 })();
